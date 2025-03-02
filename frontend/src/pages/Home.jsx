05c9b0ecc936ecc9309fc54b33d89ab23c247488
TheUnnamedOne1 <jesse.bou@outlook.com>
2025-03-02 02:13:28 -0500

    backend auth

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+// frontend/src/pages/Home.jsx
+
+import React from 'react';
+
+function Home() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Welcome to My Minecraft Hosting Service</h1>
+      <p>
+        This is the homepage of our Minecraft hosting platform. 
+        Feel free to explore our features and set up your own server!
+      </p>
+    </div>
+  );
+}
+
+export default Home;
